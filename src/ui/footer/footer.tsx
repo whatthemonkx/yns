177821a96360a6302b8ba9aeddd4eb56a655d9f7
h5371h <htsd18@gmail.com>
2024-10-17 08:47:15 -0700

    fix: minor

--- src/ui/footer/footer.tsx
+++ src/ui/footer/footer.tsx
@@ -68,7 +68,7 @@ export async function Footer() {
 			</div>
 			<div className="container mt-8 flex max-w-7xl flex-col items-center justify-between gap-4 text-sm text-neutral-500 md:flex-row">
 				<div>
-					<p>© 2024 Your Next Store</p>
+					<p>© 2024 WHAT THE MONK</p>
 					<p>Delightfully commerce for everyone</p>
 				</div>
 				<div className="flex items-center gap-4">
--- src/ui/nav/nav.tsx
+++ src/ui/nav/nav.tsx
@@ -28,7 +28,7 @@ export const Nav = async () => {
 		<header className="border-b py-4">
 			<div className="sm:items-centerm mx-auto flex max-w-7xl flex-col items-start gap-2 px-4 sm:flex-row sm:flex-wrap sm:items-center sm:px-6 md:flex-nowrap lg:px-8">
 				<YnsLink href="/">
-					<SeoH1 className="-mt-0.5 whitespace-nowrap text-xl font-bold">Your Next Store</SeoH1>
+					<SeoH1 className="-mt-0.5 whitespace-nowrap text-xl font-bold">WHAT THE MONK</SeoH1>
 				</YnsLink>
 
 				<div className="sm:mr-auto">
